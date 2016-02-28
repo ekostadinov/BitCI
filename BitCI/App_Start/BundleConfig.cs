6c9e70a92bad2201fc87091ebd7a4b5c8d4265d5
evkostadinov <evgeni.kostadinov@bedegaming.com>
2016-02-28 17:54:31 +0200

    remove build controller tests

--- BitCI/App_Start/BundleConfig.cs
+++ BitCI/App_Start/BundleConfig.cs
@@ -23,7 +23,6 @@ namespace BitCI
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            //todo: may apply your custom style changes here
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
