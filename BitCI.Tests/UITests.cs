91513323848ee869297c2c58b106761911afdf90
evkostadinov <evgeni.kostadinov@bedegaming.com>
2016-03-04 10:59:14 +0200

    add login UI tests

--- BitCI.Tests/UITests.cs
+++ BitCI.Tests/UITests.cs
@@ -22,7 +22,8 @@ namespace BitCI.Tests
         private IWebElement _loginUsernameInput;
         private IWebElement _loginPasswordInput;
         private IWebElement _loginButton;
-
+        private IWebElement _logoffLink;
+        private IWebElement _invalidLoginText;
 
         [SetUp]
         public void Setup()
@@ -33,23 +34,50 @@ namespace BitCI.Tests
             _driver.Navigate().GoToUrl(serverUrl);
             _driver.Manage().Window.Maximize();
 
+            // init elements
+            _loginUsernameInput = ReturnElementForDefaultWait(By.XPath("//input[@id='Email']"));
+            _loginUsernameInput.Clear();
+            _loginPasswordInput = ReturnElementForDefaultWait(By.XPath("//input[@id='Password']"));
+            _loginPasswordInput.Clear();
+            _loginButton = ReturnElementForDefaultWait(By.XPath("//input[@value='Log in']"));
+            _logoffLink = ReturnElementForDefaultWait(By.XPath("//*[text()='Log off']"));            
+            _invalidLoginText = ReturnElementForDefaultWait(By.XPath("//*[text()='Invalid login attempt.']"));
         }
 
         [Test]
         public void Valid_User_Should_Be_Able_To_Login()
         {
-            _loginUsernameInput = ReturnElementForDefaultWait(By.XPath("//input[@id='Email']"));
-            _loginUsernameInput.Clear();
+            // Act
             _loginUsernameInput.SendKeys("foo@bar.com");
-
-            _loginPasswordInput = ReturnElementForDefaultWait(By.XPath("//input[@id='Password']"));
-            _loginPasswordInput.Clear();
             _loginPasswordInput.SendKeys("Foobar12#$");
-
-            _loginButton = ReturnElementForDefaultWait(By.XPath("//input[@value='Log in']"));
             _loginButton.Click();
 
+            // Assert
+            Assert.True(_logoffLink.Displayed);
+        }
 
+        [Test]
+        public void InValid_User_Should_Not_Be_Able_To_Login()
+        {
+            // Act
+            _loginUsernameInput.SendKeys("Invalid@mail.com");
+            _loginPasswordInput.SendKeys("Foobar12#$");
+            _loginButton.Click();
+
+            // Assert
+            Assert.True(_invalidLoginText.Displayed);
+        }
+
+        [Test]
+        public void Valid_User_With_InValid_Pass_Should_Not_Be_Able_To_Login()
+        {
+            // Act
+            _loginUsernameInput.SendKeys("foo@bar.com");
+            _loginPasswordInput.SendKeys("Foobar34#$");
+            _loginButton.Click();
+
+            // Assert
+            Assert.True(_invalidLoginText.Displayed);
         }
 
         public IWebElement ReturnElementForDefaultWait(By by)
